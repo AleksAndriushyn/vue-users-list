0e370bdd9a2e77630cb503f522cb6625cdd87e82
Алексей Андрюшин <aleksroebuck@gmail.com>
2025-03-23 17:11:58 +0200

    first commit

---
+++ src/components/Preloader.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="preloaderWr">
+    <div class="preloader">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+const showDetails = ref(false);
+
+defineProps({
+  user: {
+    type: Object,
+    default: () => ({}),
+  }
+})
+
+const toggleDetails = () => {
+  showDetails.value = !showDetails.value
+}
+</script>
+
+<template>
+  <div class="userItem">
+    <div class="userInfo">
+      <strong>{{ user.name }}</strong>
+      <button @click="toggleDetails" class="detailsBtn">
+        <strong>{{ showDetails ? 'Сховати' : 'Показати' }} Деталі</strong>
+      </button>
+    </div>
+    <div class="userDetails" :class="{show: showDetails}">
+      <p>Email: {{ user.email }}</p>
+      <p>Телефон: {{ user.phone }}</p>
+    </div>
+  </div>
+</template>
